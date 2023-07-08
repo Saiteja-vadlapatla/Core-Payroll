398b09ea07a2ea651e931a224ce724bd9e5597b9
Sai Teja <saitejavadlapatla@gmail.com>
2023-07-08 22:20:54 +0530

    Attendance and OT data for wages

--- src/renderer/Pages/Wages.js
+++ src/renderer/Pages/Wages.js
@@ -40,6 +40,7 @@ const Wages = () => {
     []
   );
   const [attendanceData, setAttendanceData] = React.useState([]);
+  const [otData, setOtData] = React.useState([]);
 
   useEffect(() => {
     // Get Active Employee List
@@ -81,6 +82,15 @@ const Wages = () => {
       return temp;
     };
 
+    const getOTData = async () => {
+      const querySnapshot = await getDocs(collection(db, 'otData'));
+      const temp = [];
+      querySnapshot.forEach((doc) => {
+        temp.push({ ...doc.data(), id: doc.id });
+      });
+      return temp;
+    };
+
     getActiveEmployeeList().then((data) => {
       console.log('data', data);
       setActiveEmployeeList(data);
@@ -91,6 +101,11 @@ const Wages = () => {
     getAttendanceData().then((data) => {
       console.log('data', data);
       setAttendanceData(data);
+    });
+
+    getOTData().then((data) => {
+      console.log('data', data);
+      setOtData(data);
     });
   }, []);
 
@@ -171,22 +186,43 @@ const Wages = () => {
             const attendance =
               employeeAttendanceInfo[currentYear][currentMonth];
             Object.keys(attendance).forEach((key) => {
-              if (attendance[key] === 'P' || attendance[key] === 'H') {
+              if (attendance[key] === 'F' || attendance[key] === 'P') {
                 employee.noOfDaysWorked += 1;
+              } else if (attendance[key] === 'H') {
+                employee.noOfDaysWorked += 0.5;
               }
             });
           }
         }
-        employee.allowances = 0;
+        // Get OT hours from OT data
+        const employeeOTInfo = otData.find(
+          (data) => data.employeeId === employee.employeeId
+        );
         employee.otHours = 0;
+        if (employeeOTInfo[currentYear]) {
+          // OT hour is marked per day. So, add all OT hours in currentMonth
+          const ot = employeeOTInfo[currentYear][currentMonth];
+          if (ot) {
+            Object.keys(ot).forEach((key) => {
+              employee.otHours += ot[key];
+            });
+          }
+        }
+
+        employee.allowances = 0;
         // OT amount is gross wage / no. of days in month / 8
         // Calculate no. of days in currentMonth without using any library
-        const daysInMonth = new Date(currentYear, currentMonth, 0).getDate();
+        const daysInMonth = new Date(
+          currentYear,
+          monthsMap[currentMonth] + 1,
+          0
+        ).getDate();
         employee.otAmount =
           Math.round(
             (employee.grossWage / daysInMonth / 8) * employee.otHours
           ) || 0;
         // add incentive if attendance is 100%. Attendance is 100% if noOfDaysWorked = daysInMonth
+
         if (employee.noOfDaysWorked === daysInMonth) {
           employee.incentives = 200;
         } else {
